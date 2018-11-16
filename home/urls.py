e646ab32680b250e8a45a14d7ad0a86ec29bd5de
Joshua Simpson <simpson.joshua2@gmail.com>
2018-11-16 14:23:55 -0700

    Fixed a bug

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('assets/', views.assets, name='assets'),
     path('employees/', views.employees, name='employees'),
-    path('manufacturers/'), views.manufacturers, name='manufacturers'),
+    path('manufacturers/', views.manufacturers, name='manufacturers'),
     path('locations/'), views.locations, name='locations'.
 ]
