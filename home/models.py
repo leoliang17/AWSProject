c5ef68651a4e722509ec41fbdf8071fce2e22cfe
Joshua Simpson <simpson.joshua2@gmail.com>
2018-11-16 14:08:49 -0700

    Added more models

--- home/models.py
+++ home/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=100)
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
 class Employee(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     position = models.CharField(max_length=30)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
 class Asset(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
-    currentLocation = models.CharField(max_length=100)
+    currentLocation = models.ForeignKey(Location, on_delete=models.CASCADE)
     orgTag = models.CharField(max_length=20)
-    manufacturer = models.CharField(max_length=30)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     manufacturerPartNum = models.IntegerField()
     description = models.CharField(max_length=500)
     dateImplemented = models.DateField()
     maintNotes = models.CharField(max_length=500)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+
+
