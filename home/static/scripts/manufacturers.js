894e3f039e0c9b8bbe7788526819cdd365b63bed
jharris1829 <jharris1829@gmail.com>
2018-11-18 21:59:43 -0700

    added datatables and styling

---
+++ home/static/scripts/manufacturers.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // Activate the data table and provide confirm box deleting products
+    $('#sort').dataTable({
+        "columnDefs": [
+            { "orderable": false, "targets": 3 }
+        ]
+    });
+
+    $('.delete_btn').click(function() {
+        if (confirm("Are you sure you want to delete this product?")) {}
+        else {return false}
+    })
+
+});
